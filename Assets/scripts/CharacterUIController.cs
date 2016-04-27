79a8b4d6b70707c102e875c4b98b4e0f56fb760c
Elijahmon <elijahmon@live.com>
2016-04-27 13:58:35 -0400

    placeholderUI work

---
+++ Assets/scripts/CharacterUIController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterUIController : UIController {
+
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    protected override bool Initialize()
+    {
+        bool ret = base.Initialize();
+        //Additional Initialization here
+        return ret;
+    }
+}
---
+++ Assets/scripts/Models/UIController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class UIController : MonoBehaviour
+{
+    [SerializeField]
+    protected Canvas _renderCanvas;
+
+    protected bool active;
+
+
+    /// <summary>
+    /// Used to show or hide the UI associated with this controller by enabling the canvas
+    /// </summary>
+    /// <param name="show">show(true) or hide(false) the UI element</param>
+    /// <returns>true if a change was made false if not</returns>
+    public virtual bool ShowHide(bool show)
+    {
+        if(active != show)
+        {
+            active = show;
+            _renderCanvas.enabled = active;
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Run in Start method attempts to assign canvas
+    /// </summary>
+    /// <returns>true if canvas is assigned false if not</returns>
+    protected virtual bool Initialize()
+    {
+        _renderCanvas = this.GetComponentInParent<Canvas>();
+        return _renderCanvas != null ? true : false;
+    }
+}
